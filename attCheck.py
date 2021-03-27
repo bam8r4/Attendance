3dd9dabe03323e3fed58799ef73150b4dc53696e
bam8r <bam8r4@mst.edu>
2021-03-26 19:09:25 -0500

    Updated to check for 'LLL' instead of 'LL' also updated test cases.

--- attCheck.py
+++ attCheck.py
@@ -1,18 +1,17 @@
 def checkString(mystring):
-    acount = 0    
+    acount = 0
     for x in range(0,len(mystring)):
         if(mystring[x]=="A"):
             acount += 1
-            
-    if(acount >=  2 or mystring.find("LL") > -1):
-        
+
+    if(acount >=  2 or mystring.find("LLL") > -1):
+
         print("False")
     else:
         print("True")
-    
 
-mylist = ['PPPPPP','PAPALL','PPALLL','PPALLP','PLPALL','PPALPP']    
+
+mylist = ['PPALLP','PPALLL','PAPALL','PLPALL']
 
 for x in mylist:
     checkString(x)
-        
